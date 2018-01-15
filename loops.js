8c0ba153cfde58213f0db62eade44db4581adb4e
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-15 04:41:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,10 @@
-for (let i = 0; i < 25; i++) {
-  // ...
+function forLoop(newArray) {
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+      newArray[i] = 
+    }
+    else {
+      
+    }
+  }
 }
