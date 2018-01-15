68ddabd9d335023db49c26629493657cc2e5860c
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-01-15 04:41:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(newArray) {
   for (let i = 0; i < 25; i++) {
     if (i == 1) {
-      newArray[i] = 
+      newArray[i] = "I am 1 strange loop."
     }
     else {
-      
+      newArray[i] = 
     }
   }
 }
